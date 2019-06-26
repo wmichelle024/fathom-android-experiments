e073b03552bb094463f20aca1dd9561a4733a405
Severin Rudie <srudie@mozilla.com>
2019-06-26 16:27:03 -0700

    mostly hooked up index.js to trainees

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-const {dom, flavor, rule, ruleset} = require('fathom-web');
+//const {dom, flavor, rule, ruleset} = require('fathom-web');
+import { dom, flavor, rule, ruleset } from 'fathom-web';
+import trainees from './trainees.js';
 
-function containsColonsOrDashes(elem) {
-    return true;
-}
 
-// example partially from https://github.com/mozilla/fathom
-var titleFinder = ruleset(
-    // Give any title tag a score of 1, and tag it as title-ish:
-    rule(dom("title"), node => [{score: 1, flavor: 'titley'}])
-);
 
-var titleNode = titleFinder.score(document).max('titley').element;
-var title = titleNode.innerText;
+//function containsColonsOrDashes(elem) {
+//    return true;
+//}
+//
+//// example partially from https://github.com/mozilla/fathom
+//var titleFinder = ruleset(
+//    // Give any title tag a score of 1, and tag it as title-ish:
+//    rule(dom("title"), node => [{score: 1, flavor: 'titley'}])
+//);
+//
+//var titleNode = titleFinder.score(document).max('titley').element;
+//var title = titleNode.innerText;
 
-java.setTitle(title);
+const rules = trainees.ruleset();
+const target = rules.against(dom);
+
+
+java.setTitle(target);
